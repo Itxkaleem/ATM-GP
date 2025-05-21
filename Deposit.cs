f4eb74b271a7d26eff1f8329027e7a93009de2a9
Itxkaleem <165987563+Itxkaleem@users.noreply.github.com>
2025-05-21 15:24:00 +0500

    Add files via upload

---
+++ Deposit.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_management_system_GP
+{
+    public partial class Deposit : Form
+    {
+        public Deposit()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\ATMdb.mdf;Integrated Security=True;Connect Timeout=30");
+        string AccNum = LogIn.AccNum;
+        //string trtype = "Deposit";
+            private void addtransaction()
+            {
+                string trtype = "Deposit";
+            try
+                {
+                    con.Open();
+                    String query = "INSERT INTO Transactiontbl VALUES ('" + AccNum + "', '" + trtype + "', '" + DepositAmttb.Text + "', '" + DateTime.Today.Date.ToString() + "')";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+                    //MessageBox.Show("Account Created Successfully");
+                    con.Close();
+                    LogIn log = new LogIn();
+                    log.Show();
+                    this.Hide();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            
+
+            }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DepositAmttb.Text == "" || Convert.ToInt32(DepositAmttb.Text) <= 0)
+            {
+                MessageBox.Show("Please Enter Amount to Deposit");
+            }
+            else
+            {   
+                newbalance= oldbalance + Convert.ToInt32(DepositAmttb.Text);
+                try
+                {
+                    con.Open();
+                    string query = "UPDATE AccountTbl SET Balance = " +newbalance+" WHERE AccNo = "+AccNum+ "" ;
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Amount Deposited Successfully");
+                    con.Close();
+
+                    addtransaction();
+                    Home home = new Home();
+                    home.Show();
+                    this.Hide();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            Home home=new Home();
+            home.Show();
+            this.Hide(); 
+        }
+        int oldbalance,newbalance;
+        private void getbalance()
+        {
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT Balance FROM AccountTbl WHERE AccNo =" + AccNum + "", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            oldbalance = Convert.ToInt32( dt.Rows[0][0].ToString());
+            con.Close();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void Deposit_Load(object sender, EventArgs e)
+        {
+            getbalance();
+        }
+    }
+}
